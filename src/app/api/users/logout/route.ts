6f0d7dd934af3b3c271d452869559cfb02661db6
DHIRAJKumar09 <dhirajsinghania09@gmail.com>
2024-10-11 19:06:26 +0530

    changes

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,23 +1,25 @@
-import {connect} from '@/dbConfig/dbConfig';
-
-import {NextRequest,NextResponse} from 'next/server'
-
+import { connect } from '@/dbConfig/dbConfig';
+import { NextRequest, NextResponse } from 'next/server';
 
 connect();
 
-export async  function GET(request:NextRequest){
-    try{
+export async function GET(request: NextRequest) {
+    try {
+        // Create the response object
         const response = NextResponse.json({
-            message:"Logout successfully",
-            success:true,
-        })
-        response.cookies.set("token","",{
-            httpOnly:true,
-            expires:new Date(0)
-        })
-        return response;
+            message: "Logout successfully",
+            success: true,
+        });
+        
+        // Set the token cookie to expire
+        response.cookies.set('token', '', {
+            httpOnly: true,
+            expires: new Date(0), // Expire the cookie
+        });
+        
+        return response; // Return the response
 
-    }catch(error:any){
-        return NextResponse.json({error:error.message},{status:500})
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
